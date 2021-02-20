bf1c712c1f3ca6c1db896fe203f54783acf5bfa9
marineboy308 <35655841+marineboy308@users.noreply.github.com>
2021-02-20 19:10:07 +0000

    Update domains.js

--- modules/domains.js
+++ modules/domains.js
@@ -24,7 +24,47 @@ const UserModel = Mongoose.model("users", UserSchema)
     })
 }
 
+function validatePassword(password) {
+    if (password.length < 8) {
+        return {
+            valid: false,
+            reason: "Password must be at least 8 characters long"
+        }
+    } else if (password.toUpperCase() == password || password.toLowerCase() == password) {
+        return {
+            valid: false,
+            reason: "Password must include uppercase and lowercase characters"
+        }
+    } else if (!/\d/g.test(password) || !/[a-zA-Z]/g.test(password)) {
+        return {
+            valid: false,
+            reason: "Password must contain letters and numbers"
+        }
+    }
+
+    return {
+        valid: true
+    }
+}
+
 module.exports = [
+    {
+        path: "captcha-project-validate-password",
+        type: "POST",
+        method: async (request, resolve) => {
+            let userValidation = request.header("X-User-Validation")
+            if (!userValidation) return resolve.status(400).send("Expects user validation");
+        
+            if (!request.header("X-Access-Token")) return resolve.status(499).send("An access token is required for this request")
+            if (request.header("X-Access-Token") != process.env.ACCESSTOKEN) return resolve.status(498).send("An invalid access token was provided");
+
+            userValidation = JSON.parse(userValidation)
+            if (!(userValidation instanceof Object)) return resolve.status(400).send("User validation must be JSON object");
+            if (!userValidation.password) return resolve.status(400).send("User validation must the password");
+
+            resolve.send(JSON.stringify(validatePassword(userValidation.password)))
+        }
+    },
     {
         path: "captcha-project-validate-login",
         type: "POST",
@@ -44,7 +84,7 @@ module.exports = [
                 Password: userValidation.password
             })
 
-            if (data) {
+            if (data) { 
                 resolve.send(JSON.stringify({
                     exists: true
                 }))
@@ -68,6 +108,12 @@ module.exports = [
             userContent = JSON.parse(userContent)
             if (!(userContent instanceof Object)) return resolve.status(400).send("User content must be JSON object");
             if (!userContent.firstname || !userContent.lastname || !userContent.password) return resolve.status(400).send("User content must contain firstname, lastname and password");
+
+            let result = validatePassword(userContent.password)
+            if (!result.valid) {
+                result.created = false
+                return resolve.send(JSON.stringify(result))
+            }
 
             let username = userContent.firstname.substr(0, 1).toLowerCase() + userContent.lastname.toLowerCase()
 
